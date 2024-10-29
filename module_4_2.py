caa158325761a50f68021ca7b85f0a4fbd6d599a
Kutekime <santa55555@mail.ru>
2024-10-29 16:54:34 +0300

    Delete module_4_2.py

--- module_4_2.py
+++
@@ -1,7 +0,0 @@
-def test_function():
-    def inner_function():
-        print("Я в области видимости функции test_function")
-    inner_function()
-
-test_function() #ok, inner_function была вызвана
-inner_function() #нет такой функции в глобальной области видимости
