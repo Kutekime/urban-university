986973fa5bc10e33d7d21dd1e625b0ec72bf6afb
Kutekime <santa55555@mail.ru>
2024-11-07 23:24:15 +0300

    Update main_8_1.py

--- main_8_1.py
+++ main_8_1.py
@@ -1,36 +1,12 @@
 def add_everything_up(a, b):
-    return a + b
-
-try:
-    print(add_everything_up(123.456, 'строка'))
-    #код ниже не будет выполняться
-    print(add_everything_up('яблоко', 4215))
-    print(add_everything_up(123.456, 7))
-except TypeError as exc:
-    print(f'Произошла ошибка: {exc}')
-    print(f'Вы пытаетесь сложить два числа! Ладно, в этот раз сложим:')
-    print(f'{str(4215) + " строка"}')
-else:
-    print('Эх, до меня дело не дойдёт :(')
-finally:
-    print('Так или иначе код выполнили!\n')
-
-try:
-    print(add_everything_up('яблоко', 4215))
-except TypeError as exc:
-    print(f'Произошла ошибка: {exc}')
-    print('Вы пытаетесь сложить два числа! Ладно, в этот раз сложим:')
-    print(f'{"яблоко " + str(4215)}')
-else:
-    print('Эх, до меня дело не дойдёт :(')
-finally:
-    print('Так или иначе код выполнили!\n')
-
-try:
-    print(add_everything_up(123.456, 7))
-except TypeError as exc:
-    print('Эх, теперь до меня дело не дойдёт >:(')
-else:
-    print('Ошибка не допущена!')
-finally:
-    print('Так или иначе код выполнили!')
+    try:
+        a + b
+    except TypeError as exc:
+        print(f'Произошла ошибка: {exc}')
+        print('Вы пытаетесь сложить что-то отличное от чисел! Ладно, в этот раз сложим..')
+        return str(a) + str(b)
+    else:
+        print('Ошибка не допущена!')
+        return a + b
+    finally:
+        print('Так или иначе код выполнили!')
